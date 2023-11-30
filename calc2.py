6975bd5f79bc5b7f1a9417814f76506dbcecb096
Bruno Ferrari Toniatti <128197225+BrunoToniatti@users.noreply.github.com>
2023-11-30 14:25:57 -0300

    Update calc2.py
    Voltando ao normal

--- calc2.py
+++ calc2.py
@@ -1,18 +1,10 @@
 # calculadora simples otimizada
 
-
 def soma(n1, n2):
     return n1 + n2
 
 def sub(n1, n2):
     return n1 - n2
-
-def multi(n1, n2):
-    return n1 * n2
-
-def div(n1, n2):
-    return n1 / n2
-
 
 # teste
 
